8b846bf8a41d45e7f9247b1cab15b55050a6c096
Joao Gomes <joao.victor214@gmail.com>
2022-10-09 15:17:28 -0300

    Fazendo o pedido do 06

--- 06-festival/src/pedido.cpp
+++ 06-festival/src/pedido.cpp
@@ -1,1 +1,30 @@
-// TODO implemente essa classe de acordo com o hpp correspondente
+#include "pedido.hpp"
+
+void Pedido::adicionaProduto(Produto *p) {
+	_produtos.push_back(p);
+};
+
+void Pedido::setEndereco(const std::string &endereco) {
+	_endereco = endereco;
+};
+
+float Pedido::calculaTotal() const {
+	float precoPedido = 0;
+
+	for (Produto *p : _produtos) {
+		precoPedido = precoPedido + p->calcPreco()
+	};
+
+	return precoPedido;
+};
+
+std::string Pedido::resumo() const {
+	std::string resumoPedido = "";
+
+	for (Produto *p : _produtos) {
+		resumoPedido = p->descricao();
+		resumoPedido = "\n";
+	};
+
+	return resumoPedido;
+};
