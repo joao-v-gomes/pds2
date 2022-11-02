3b66c0ab8bb5cb533add6dc48dc3a0d07affc85f
Joao Gomes <joao.victor214@gmail.com>
2022-11-02 11:22:26 -0300

    Adicionando o vpl7

--- 07-caixa/include/intruso.hpp
+++ 07-caixa/include/intruso.hpp
@@ -1,16 +1,12 @@
+#include <iterator>
 #include <list>
+#include <map>
 #include <string>
-
-class SenhaVazada {
-	std::string _numeros;
-	std::string _digitos;
-
-	SenhaVazada(std::string numeros, std::string digitos);
-};
+#include <vector>
 
 class Intruso {
 	/*Continue a implementação da classe Intruso*/
-	std::list<SenhaVazada> lista_senhas_vazadas;
+	std::vector<std::vector<std::string>> lista_relacao_letras_numeros;
 
   public:
 	void set_senha_vazada(std::string vazou);
--- 07-caixa/src/intruso.cpp
+++ 07-caixa/src/intruso.cpp
@@ -1,28 +1,162 @@
 #include "intruso.hpp"
 #include <iostream>
 
-SenhaVazada::SenhaVazada(std::string numeros, std::string digitos) {
-	_numeros = numeros;
-	_digitos = digitos;
-};
-
 void Intruso::set_senha_vazada(std::string vazou) {
+	std::string aux_vazou;
 	std::string aux_numeros;
 	std::string aux_digitos;
+	std::string aux_relacao_letras_numeros;
+	std::vector<std::string> relacao_letras_numeros;
 
-	for (int i = 0; i < 10; i++) {
-		aux_numeros = aux_numeros + vazou[i];
+	// Pega entrada e retira os espacos
+	for (uint i = 0; i < vazou.size(); i++) {
+		// std::cout << "Vazou: " << vazou[i] << "\r\n";
+		if (vazou[i] != ' ') {
+			aux_vazou = aux_vazou + vazou[i];
+		};
 	};
 
-	std::cout << aux_numeros << "\r\n";
+	// std::cout << aux_vazou << "\r\n";
 
-	for (int i = 10; i < '\0'; i++) {
-		aux_digitos = aux_digitos + vazou[i];
+	// Pega a relacao de numeros da entrada sem espacos
+	for (uint i = 0; i < 10; i++) {
+		if (aux_vazou[i] != ' ') {
+			aux_numeros = aux_numeros + aux_vazou[i];
+		};
 	};
 
-	std::cout << aux_digitos << "\r\n";
+	// std::cout << aux_numeros << "\r\n";
+
+	// Pega as letras digitadas
+	for (uint i = 10; i < 16; i++) {
+		if (aux_vazou[i] != ' ') {
+			aux_digitos = aux_digitos + aux_vazou[i];
+		};
+	};
+
+	// std::cout << aux_digitos << "\r\n";
+
+	// Olha todas as letras da senha digitada e junta com os digitos numa string
+	for (uint i = 0; i < aux_digitos.size(); i++) {
+		char c;
+
+		c = aux_digitos[i];
+
+		// std::cout << "Letra: " << c << "\r\n";
+
+		switch (c) {
+			case 'A':
+				// aux_relacao_letras_numeros = aux_digitos[i] + (char)aux_numeros[0] + (char)aux_numeros[1];
+				aux_relacao_letras_numeros = aux_digitos[i];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[0];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[1];
+				break;
+			case 'B':
+				// aux_relacao_letras_numeros = aux_digitos[i] + (char)aux_numeros[2] + (char)aux_numeros[3];
+				aux_relacao_letras_numeros = aux_digitos[i];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[2];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[3];
+				break;
+			case 'C':
+				// aux_relacao_letras_numeros = aux_digitos[i] + (char)aux_numeros[4] + (char)aux_numeros[5];
+				aux_relacao_letras_numeros = aux_digitos[i];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[4];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[5];
+				break;
+			case 'D':
+				// aux_relacao_letras_numeros = aux_digitos[i] + (char)aux_numeros[6] + (char)aux_numeros[7];
+				aux_relacao_letras_numeros = aux_digitos[i];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[6];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[7];
+				break;
+			case 'E':
+				// aux_relacao_letras_numeros = aux_digitos[i] + (char)aux_numeros[8] + (char)aux_numeros[9];
+				aux_relacao_letras_numeros = aux_digitos[i];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[8];
+				aux_relacao_letras_numeros = aux_relacao_letras_numeros + aux_numeros[9];
+				break;
+			default:
+				std::cout << "Entrou default";
+				break;
+		}
+
+		// std::cout << "String: " << aux_relacao_letras_numeros << "\r\n";
+
+		// Caso a string nao esteja vazia, da um push_back no vetor de strings
+		if (aux_relacao_letras_numeros != "") {
+			relacao_letras_numeros.push_back(aux_relacao_letras_numeros);
+			aux_relacao_letras_numeros = "";
+		} else {
+			std::cout << "Relacao vazia";
+		}
+	}
+
+	// Add o vetor de strings no vetor de vetor de string
+	lista_relacao_letras_numeros.push_back(relacao_letras_numeros);
+
+	// std::cout << "Adicionou uma nova senha vazada a lista \r\n";
 };
 
 std::string Intruso::crack_senha() {
-	return 0;
-};
+
+	// Senha final sem espacos
+	std::string senha_crakeada = "      ";
+
+	// Senha final com espacos
+	std::string senha_crakeada_final = "";
+
+	// Pega a primeira relacao de letras e digitos
+	std::vector<std::string> aux_senha = lista_relacao_letras_numeros[0];
+
+	// for (int i = 0; i < lista_relacao_letras_numeros.size(); i++) {
+	// 	for (int j = 0; j < lista_relacao_letras_numeros[i].size(); j++) {
+	// 		std::cout << lista_relacao_letras_numeros[i][j] << "\r\n";
+	// 	}
+
+	// 	std::cout << "\r\n";
+	// }
+
+	// Percorre o vetor de vetor de strings
+	for (uint i = 1; i < lista_relacao_letras_numeros.size(); i++) {
+		// Percorre cada digito do vetor de strings
+		for (uint k = 0; k < aux_senha.size(); k++) {
+			// std::cout << "Comparacao: " << aux_senha[k] << " e " << lista_relacao_letras_numeros[i][k] << "\r\n";
+
+			// Caso os dois digitos sejam iguais, nao faz nada.
+			// Caso somente um seja igual, adiciona ele na posicao pesquisada em aux_senha
+			if (((aux_senha[k][1] == lista_relacao_letras_numeros[i][k][1]) && (aux_senha[k][2] == lista_relacao_letras_numeros[i][k][2])) ||
+				((aux_senha[k][1] == lista_relacao_letras_numeros[i][k][2]) && (aux_senha[k][2] == lista_relacao_letras_numeros[i][k][1]))) {
+				// std::cout << "Os dois numeros sao iguais!\r\n";
+			} else if ((aux_senha[k][1] == lista_relacao_letras_numeros[i][k][1]) && (aux_senha[k][2] != lista_relacao_letras_numeros[i][k][2])) {
+				// std::cout << "Primeiro igual ao primeiro!\r\n";
+				senha_crakeada[k] = aux_senha[k][1];
+			} else if ((aux_senha[k][1] != lista_relacao_letras_numeros[i][k][1]) && (aux_senha[k][2] == lista_relacao_letras_numeros[i][k][2])) {
+				// std::cout << "Segundo igual ao segundo!\r\n";
+				senha_crakeada[k] = aux_senha[k][2];
+			} else if ((aux_senha[k][1] != lista_relacao_letras_numeros[i][k][2]) && (aux_senha[k][2] == lista_relacao_letras_numeros[i][k][1])) {
+				// std::cout << "Segundo igual ao primeiro!\r\n";
+				senha_crakeada[k] = aux_senha[k][2];
+			} else if ((aux_senha[k][2] != lista_relacao_letras_numeros[i][k][1]) && (aux_senha[k][1] == lista_relacao_letras_numeros[i][k][2])) {
+				// std::cout << "Primeiro igual ao segundo!\r\n";
+				senha_crakeada[k] = aux_senha[k][1];
+			} else {
+				// std::cout << "Os dois sao diferentes!\r\n";
+			}
+
+			// std::cout << "Senha parcial: " << senha_crakeada << "\r\n";
+		}
+	}
+
+	// std::cout << "Senha: " << senha_crakeada << "\r\n";
+
+	// Adiciona os espacos na senha final
+	for (uint i = 0; i < senha_crakeada.size(); i++) {
+		senha_crakeada_final = senha_crakeada_final + senha_crakeada[i];
+		senha_crakeada_final = senha_crakeada_final + ' ';
+	}
+
+	// Remove o ultimo espaco adicionado
+	senha_crakeada_final.pop_back();
+
+	return senha_crakeada_final;
+}
